7ab7b85e30a55e5de55e13a1e27ff85528df623d
Kenny Hom <kenny.hom27@gmail.com>
2017-11-20 18:41:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! + ":"
+    puts holiday.to_s.split("_").map(&:capitalize).join(" ")
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
